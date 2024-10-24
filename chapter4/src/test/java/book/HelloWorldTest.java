ae806d6157732dccdc0b7cd52860d6634a858aa7
robert0714 <robert0714@gmail.com>
2024-10-24 22:16:14 +0800

    adjust ch4 integration test

--- chapter4/src/test/java/book/HelloWorldTest.java
+++ chapter4/src/test/java/book/HelloWorldTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.util.ByteBufferBackedInputStream;
+import org.testcontainers.junit.jupiter.Testcontainers; 
 import org.testcontainers.utility.DockerImageName; 
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -13,13 +12,8 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.SdkBytes; 
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient; 
-
-import org.json.JSONObject;
-
-import java.io.File;
-import java.io.FileInputStream;  
-import java.io.IOException;
-import java.nio.ByteBuffer;
+ 
+ 
 import java.nio.charset.StandardCharsets; 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,19 +42,7 @@ public class HelloWorldTest {
 	       		 .region(Region.of(localStack.getRegion())) 
 	             .build();
 	   }
-
-		protected String functionCode() {
-			File file = new File("src/main/java/book/HelloWorld.java");
-			String content = null;
-			try (FileInputStream fis = new FileInputStream(file);) {
-				content = new String(fis.readAllBytes(), StandardCharsets.UTF_8);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			return content;
-		}
+ 
 
 	   @Test
 	   public void testHandler() throws Exception {
@@ -90,25 +72,13 @@ public class HelloWorldTest {
 	       System.out.println("*** Sleep for 1 min to get Lambda function ready.");       
 	       SDKv2LambdaUtils. waitForFunctionActive(lambdaClient , functionName);       
 	       
-	    // Need a SdkBytes instance for the payload.
-	       
-	    // SdkBytes payload = pseudoPayload();
+	    // Need a SdkBytes instance for the payload. 
 		   SdkBytes payload = SdkBytes.fromString("\"World\"", StandardCharsets.UTF_8);
 	       
 	       String  result = SDKv2LambdaUtils.invokeFunction(lambdaClient, functionName ,payload);
 	       System.out.println(DASHES);
 	       
 	       assertEquals("\"Hello, World\"", result);
-	   }
-
-		protected SdkBytes pseudoPayload() {
-			JSONObject jsonObj = new JSONObject();
-			jsonObj.put("inputValue", "2000");
-			String json = jsonObj.toString();
-			SdkBytes payload = SdkBytes.fromUtf8String(json);
-			return payload;
-		}   
-
-      
+	   } 
     
  } 
